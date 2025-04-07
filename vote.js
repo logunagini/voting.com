bc6146022c04fb797929c878d1c31d9f76b0798b
logunagini <logeshwari515logu@gmail.com>
2025-04-07 20:19:29 +0530

    thank msg

--- vote.js
+++ vote.js
@@ -78,7 +78,7 @@ onAuthStateChanged(auth, (user) => {
       if (result.success) {
         alert("Your vote has been recorded successfully!");
        
-        window.location.href = "/thanking.html"; // Redirect after successful vote      } else {
+        window.location.href = "thanking.html"; // Redirect after successful vote      } else {
        ;
         } else {
         alert(result.message);
